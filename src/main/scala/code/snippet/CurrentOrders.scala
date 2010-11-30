8f91ad0bae5bb7c41ef3dcb4ef804c6a100732eb
Brent Jacobson <bjacobson@pc746-ubu.gnet.global.vpn>
2010-11-30 16:17:03 +1300

    added lots of stuff

--- src/main/scala/code/snippet/CurrentOrders.scala
+++ src/main/scala/code/snippet/CurrentOrders.scala
@@ -6,19 +6,19 @@ import Helpers._
 import net.liftweb.http.js.{JsCmd, JsCmds}
 import xml.NodeSeq
 import net.liftweb.http.js.JsCmds.Replace
-import code.model.{Order, Curry, Heat}
 import net.liftweb.http.{S, RequestVar, TemplateFinder, SHtml}
-import net.liftweb.mapper.Descending
+import code.model.{User, Order, Curry, Heat}
+import net.liftweb.mapper.{By, By_>, Descending}
 
 class CurrentOrders {
-  val orders = Order.findAll() 
-
+  val orders = Order.findAll(By_>(Order.timeStamp, new java.util.Date(new java.util.Date().getTime - (3*24*60*60*1000))))
+  
   def list = ".order" #> rows & ClearClearable
 //  def order: (NodeSeq => NodeSeq) = ".orders" #>  & ClearClearable
 
   def rows: (NodeSeq => NodeSeq) = (ns: NodeSeq) => orders.flatMap(row(_).apply(ns))
 
-  def row(o: Order) = ".name *" #> o.user.is &
+  def row(o: Order) = ".name *" #> (User.find(By(User.id, o.user.is)).map(_.realName.is) openOr "?") &
           ".curry *" #> (o.curry.obj.map(_.name.is) openOr "?") &
           ".heat *" #> o.heat
 }
