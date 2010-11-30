7808c37020f71e02c7feb91aa39082266ff95f0f
Brent Jacobson <bjacobson@pc746-ubu.gnet.global.vpn>
2010-11-30 14:53:35 +1300

    lots of changes

--- src/main/scala/code/snippet/OrderSnippet.scala
+++ src/main/scala/code/snippet/OrderSnippet.scala
@@ -3,23 +3,27 @@ package code.snippet
 import _root_.net.liftweb.util._
 import _root_.net.liftweb.common._
 import Helpers._
-import code.model.{Curry, Heat}
 import net.liftweb.http.js.{JsCmd, JsCmds}
-import net.liftweb.http.{TemplateFinder, SHtml}
 import xml.NodeSeq
 import net.liftweb.http.js.JsCmds.Replace
+import net.liftweb.http.{RequestVar, TemplateFinder, SHtml}
+import code.model.{Order, Curry, Heat}
 
 class OrderSnippet {
   val curryList = Curry.findAll()
+  object heat extends RequestVar[Heat.Heat](Heat.mildMedium)
+  object dish extends net.liftweb.http.SessionVar[Box[Curry]](Empty)
 
-  def order = ".hotness" #> SHtml.select(Heat.values.toSeq.map(e => (e.toString, e.toString)), Empty, (s: String) => _) &
+  def order = ".hotness" #> SHtml.select(Heat.values.toSeq.map(e => (e.toString, e.toString)), Empty, (s: String) => heat(Heat.withName(s))) &
           ".dish" #> SHtml.ajaxSelect(curryList.toSeq.map(e => (e.id.is.toString, e.name.is)), Empty, updateDescription _) &
-          ".sendOrder" #> SHtml.submit("order", () => JsCmds.Noop)
+          ".sendOrder" #> SHtml.submit("order", processOrder _)
 
 
   def updateDescription(s: String): JsCmd = {
+
     Curry.find(s) match {
       case Full(aCurry) =>
+        dish(Full(aCurry))
         val result = ("#pic [src]" #> aCurry.picUrl &
                 "#pic [alt]" #> aCurry.name &
                 "#desc *" #> aCurry.description &
@@ -27,6 +31,18 @@ class OrderSnippet {
         Replace("descriptionTable", result)
       case _ => JsCmds.Noop
     }
+  }
+
+  def processOrder(): JsCmd = {
+    val order: Order = Order.create
+    order.timeStamp(new java.util.Date())
+    order.user(Empty)
+    order.curry(dish.is)
+    order.heat(heat)
+
+    order.save
+
+    JsCmds.Noop
   }
 
   lazy val descriptionPart: NodeSeq = TemplateFinder.findAnyTemplate("order" :: Nil) match {
